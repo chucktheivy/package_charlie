b73ce83ab8480d6cbbe46548db2b19523042a39b
chucktheivy <charliewilson@ucsb.edu>
2023-03-15 10:35:23 -0700

    finished help for data

---
+++ R/allSweptAreaspower.R
@@ -0,0 +1,13 @@
+#' Windmill data
+#'
+#' Data generated to be used with function windmill_power
+#'
+#' @format a data from with 200 rows and 4 columns representing different wind speeds
+#' \itemize{
+#'  \item data wind speed m/s
+#'  \item data sweep area m^2
+#'  \item power data kWh
+#'  }
+#'  @source Charles Louis Wilson IV
+#'
+"allSweptAreaspower"
